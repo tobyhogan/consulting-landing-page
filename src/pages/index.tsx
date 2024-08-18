606997146e72d91b36689a4ee9472e8f99eacb59
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-08-18 17:29:47 +0100

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,13 +27,13 @@ const IndexPage: React.FC<PageProps> = () => {
       <Header />
       <main className="pt-20 pb-10">
         <img src={projectimage} className="border-2 border-slate-300 dark:border-slate-600 mt-12 rounded-lg"></img>
-        <h1 className="text-center mt-8 text-2xl">Consulting for Business and Small Business Based in Frome, Somereset</h1>
+        <h1 className="text-center mt-8 text-2xl">Consulting for Business and Small Business, <br /> Based in Frome, Somereset</h1>
         <div className="flex flex-col mx-auto w-fit mt-8">
           <a className='Button2 rounded-md py-2 px-5 text-white dark:text-black bg-slate-500 dark:bg-[#7d87bc] hover:bg-slate-400 dark:hover:bg-[#6f79a9] hover:cursor-pointer'>Get in Touch Now!</a>
           <a href="https://habit-tracker-indol-ten.vercel.app/" className="Link2 mt-4 underline text-blue-400 mx-auto">Alternate Action</a>
         </div>
-        <h1 className="text-center text-xl mt-8">Services Offered:</h1>
-        <ul className="mx-auto w-fit mt-3">
+        <h1 className="text-center text-xl mt-8">Services:</h1>
+        <ul className="mx-auto w-fit mt-3 pl-5">
           <li>- Website Creation and Renovation</li>
           <li>- Calendar, Email and Data Management</li>
           <li>- Advertising and Marketing</li>
@@ -42,6 +42,7 @@ const IndexPage: React.FC<PageProps> = () => {
         <ul className="w-fit mx-auto flex flex-col text-center mt-2">
           <a href="https://forwardstep.org" className="Link3" target="_blank">Forward Step Tutoring</a>
           <a href="https://www.theexcelexperts.com" className="Link3" target="_blank">The Excel Experts</a>
+          <a href="" className="Link3" target="_blank">UK Kebab</a>
         </ul>
       </main>
       <Footer/>
